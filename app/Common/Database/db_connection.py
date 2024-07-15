f40b6b44a77e64d6bdb25b1c0c98b819da5be846
Farah <farahzalsafwan@gmail.com>
2024-07-15 19:12:56 +0300

    changing file name

--- app/Common/Database/db_connection.py
+++ app/Common/Database/db_connection.py
@@ -8,13 +8,9 @@ from sqlalchemy.orm import Session
 
 URL_DATABASE= "postgresql://falsafwan002:Passw0rd@localhost:5432/smart_library"
 
-engine= create_engine(URL_DATABASE) # Create a new _engine.Engine instance. engine = create_engine("postgresql+psycopg2://scott:tiger@localhost/test")
+engine= create_engine(URL_DATABASE) 
 
-session_local= sessionmaker(bind= engine , autoflush= False, autocommit = False) # generates new Session objects when called to manage interactions with the database. 
-# A session serves as a gateway to the database and provides a way to persist and retrieve Python objects to and from the database.
-# sessionmaker(bind=engine) creates a session factory bound to the specified database engine (engine).
-# autoflush=False (optional parameter) disables automatic flushing of changes to the database, which can improve performance in some cases.
-# used to control whether SQLAlchemy should automatically commit transactions for each individual operation performed within a session
+session_local= sessionmaker(bind= engine , autoflush= False, autocommit = False) 
 
 Base = declarative_base()
 
