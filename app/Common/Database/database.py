987936f86279e7f0ae16cf500dd1d9d0e5c4f154
Farah <farahzalsafwan@gmail.com>
2024-07-15 17:34:18 +0300

    added relationships

---
+++ app/Common/Database/database.py
@@ -0,0 +1,52 @@
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker
+base = declarative_base()
+class Books(base):
+    __tablename__= 'books'
+
+    title= Column(String, index=True)
+    author_id = Column(Integer, primary_key=True, index=True)
+    genre = Column(String, index=True)
+    description = Column(String, index=True)
+    author = relationship('Authors', back_populates='Books')
+
+class Authors(base):
+    __tablename__= 'authors'
+
+    name= Column(String, primary_key=True, index=True)
+    biography = Column(String, index=True)
+    genre = Column(Integer, primary_key=True, index=True)
+    description = Column(Integer, primary_key=True, index=True)
+    books = relationship('Books', back_populates='Authors')
+
+class Users(base):
+    __tablename__= 'users'
+
+    username= Column(String, primary_key=True, index=True)
+    password = Column(String, index=True)
+    role = Column(String, index=True)
+    description = Column(Integer, primary_key=True, index=True)
+    preferences = relationship('UserPreferences', back_populates='Users')
+
+
+class UserPreferences(base):
+    __tablename__= 'userpreferences'
+
+    preferences= Column(String, primary_key=True, index=True)
+    author_id = Column(Integer, primary_key=True, index=True)
+    genre = Column(Integer, primary_key=True, index=True)
+    description = Column(Integer, primary_key=True, index=True)
+    user = relationship('Users', back_populates='UserPreferences')
+    bookrelation = relationship('Books', back_populates='UserPreferences')
+
+
+DATABASE_URL = "postgresql+psycopg2://falsafwan002:your_password@localhost:5432/smartlibrary"
+
+engine = create_engine(DATABASE_URL)
+
+base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
