f11ef9a6b17f4a056ad5b53f8b117f44c2955ba4
rayikume <rayikume@gmail.com>
2024-07-16 15:37:45 +0300

    added recommendation endpoints

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI # type: ignore
 import os
 from Routes.book import router as book_router
+from Routes.recommendation import router as recommendation_router
 from typing import Annotated, List
 from Common.Database.db_connection import *
 app = FastAPI()
 
 app.include_router(book_router, prefix="/books")
+app.include_router(recommendation_router, prefix="/recommendations")
 
 @app.get("/")
 def read_root():
