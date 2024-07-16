397d07e87d22f3029df12cf645e3d09422e1c755
Farah <farahzalsafwan@gmail.com>
2024-07-16 14:33:09 +0300

    changes

--- app/Common/Database/db_connection.py
+++ app/Common/Database/db_connection.py
@@ -4,9 +4,14 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from typing import Annotated
 from sqlalchemy.orm import Session
+from models import Base 
 
-URL_DATABASE = "postgresql://aalkathami001:Passw0rd@localhost:5432/smart_library"
 
+URL_DATABASE = "postgresql://falsafwan002:Passw0rd@localhost:5432/smart_library"
+engine = create_engine(URL_DATABASE)
+
+# Create all tables
+Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
