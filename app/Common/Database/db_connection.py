c7506dfc4b0014a942680c6d88cf96ff53973482
Farah <farahzalsafwan@gmail.com>
2024-07-16 14:43:11 +0300

    changes hehe

--- app/Common/Database/db_connection.py
+++ app/Common/Database/db_connection.py
@@ -2,23 +2,15 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from typing import Annotated
-<<<<<<< HEAD
 from sqlalchemy.orm import Session
 from models import Base 
-=======
 from sqlalchemy.orm import Session , declarative_base
->>>>>>> 67a375c3d39f51f74b6cbfbc36fd431bf61bbe0e
 
-
-<<<<<<< HEAD
 URL_DATABASE = "postgresql://falsafwan002:Passw0rd@localhost:5432/smart_library"
 engine = create_engine(URL_DATABASE)
 
 # Create all tables
 Base.metadata.create_all(engine)
-=======
-engine= create_engine(URL_DATABASE) 
->>>>>>> 67a375c3d39f51f74b6cbfbc36fd431bf61bbe0e
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
