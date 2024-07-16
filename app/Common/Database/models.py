8aebb150298f1ae3a2fa9c98e04c73df2e2214c1
Farah <farahzalsafwan@gmail.com>
2024-07-16 14:51:20 +0300

    changes??

--- app/Common/Database/models.py
+++ app/Common/Database/models.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 class Author(Base):
     __tablename__ = 'authors'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-    book_id= Column(Integer, ForeignKey('books.id'), nullable=False)
     name = Column(String, nullable=False)
     biography = Column(Text, nullable=True)
-    genre = Column(String, nullable=True)
-    books = relationship('Book', back_populates='authors')
+
+
 
 class Book(Base):
     __tablename__ = 'books'
@@ -20,22 +20,21 @@ class Book(Base):
     author_id = Column(Integer, ForeignKey('authors.id'), nullable=False)
     genre = Column(String, nullable=True)
     description = Column(Text, nullable=True)
-    author = relationship('Author', back_populates='books')
+
 
 class User(Base):
     __tablename__ = 'users'
-    
     id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String, nullable=False, unique=True)
     email = Column(String, nullable=False, unique=True)
     password_hash = Column(String, nullable=False)
     role = Column(String, nullable=False)
-    preferences = relationship('UserPreference', back_populates='users')
+
 
 class UserPreference(Base):
     __tablename__ = 'user_preferences'
-    # user name have prefrence1, prefrence5
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
-    user = relationship('User', back_populates='user_preferences')
-    books = relationship('Book', back_populates='user_preferences')
+    preference = Column(Text, nullable=True)  
