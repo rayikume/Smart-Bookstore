30442dec3c0469d696f2b5298a250b8eed675223
Abrar Alkathami <Abraii6@hotmail.com>
2024-07-16 09:49:43 +0300

    edit

--- app/Common/Database/models.py
+++ app/Common/Database/models.py
@@ -4,7 +4,8 @@ from app.Common.Database.db_connection import Base
 
 class Books(Base):
     __tablename__= 'books'
-    book_id = Column(String , primary_key=True, index = True)
+
+    book_id = Column(Integer , primary_key=True, index = True)
     title= Column(String, index=True)
     author_id = Column(Integer, ForeignKey("author_id"), index=True)
     genre = Column(String, index=True)
@@ -15,9 +16,9 @@ class Authors(Base):
     __tablename__= 'authors'
 
     author_name= Column(String, primary_key=True, index=True)
+    book_id = Column(Integer , ForeignKey("author_id"), index = True)
     biography = Column(String, index=True)
-    genre = Column(Integer, index=True)
-    description = Column(Text,  index=True)
+    genre = Column(String, index=True)
     books = relationship('Books', back_populates='Authors')
 
 class Users(Base):
@@ -26,16 +27,16 @@ class Users(Base):
     username= Column(String, primary_key=True, index=True)
     password = Column(String, index=True)
     role = Column(String, index=True)
-    description = Column(Text,  index=True)
     preferences = relationship('UserPreferences', back_populates='Users')
 
 
 class UserPreferences(Base):
     __tablename__= 'userpreferences'
 
-    preferences= Column(String, primary_key=True, index=True)
-    author_id = Column(Integer, primary_key=True, index=True)
-    genre = Column(Integer, primary_key=True, index=True)
-    description = Column(Integer, primary_key=True, index=True)
+    preferences= Column(String,  index=True)
+    username = Column(String, ForeignKey("username"), index=True)
+    author_id = Column(Integer, ForeignKey("author_id"), index=True)
+    genre = Column(Integer, index=True)
+    description = Column(Text,  index=True)
     user = relationship('Users', back_populates='UserPreferences')
     bookrelation = relationship('Books', back_populates='UserPreferences')
