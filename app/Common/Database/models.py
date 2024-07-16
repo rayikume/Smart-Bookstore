774f7b720bdc41a8e0f346838977eab46ec6a05b
Abrar Alkathami <Abraii6@hotmail.com>
2024-07-16 14:47:52 +0300

    edits models file

--- app/Common/Database/models.py
+++ app/Common/Database/models.py
@@ -5,11 +5,11 @@ Base = declarative_base()
 
 class Author(Base):
     __tablename__ = 'authors'
-
     id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id= Column(Integer, ForeignKey('books.id'), nullable=False)
     name = Column(String, nullable=False)
     biography = Column(Text, nullable=True)
-
+    genre = Column(String, nullable=True)
     books = relationship('Book', back_populates='authors')
 
 class Book(Base):
@@ -20,7 +20,6 @@ class Book(Base):
     author_id = Column(Integer, ForeignKey('authors.id'), nullable=False)
     genre = Column(String, nullable=True)
     description = Column(Text, nullable=True)
-
     author = relationship('Author', back_populates='books')
 
 class User(Base):
@@ -35,10 +34,8 @@ class User(Base):
 
 class UserPreference(Base):
     __tablename__ = 'user_preferences'
-    
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    # user name have prefrence1, prefrence5
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
-    preference = Column(Text, nullable=True)  
     user = relationship('User', back_populates='user_preferences')
     books = relationship('Book', back_populates='user_preferences')
