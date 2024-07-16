5470e194af9af5c9c6beff943b645c341e0f0d76
Farah <farahzalsafwan@gmail.com>
2024-07-16 09:34:45 +0300

    edits

--- app/Common/Database/models.py
+++ app/Common/Database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey # type: ignore
-from sqlalchemy.orm import relationship, declarative_base, sessionmaker # type: ignore
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey 
+from sqlalchemy.orm import relationship, declarative_base, sessionmaker 
 from app.Common.Database.db_connection import Base
 
 class Books(Base):
@@ -8,7 +8,7 @@ class Books(Base):
     title= Column(String, index=True)
     author_id = Column(Integer, ForeignKey("author_id"), index=True)
     genre = Column(String, index=True)
-    description = Column(String, index=True)
+    description = Column(Text, index=True)
     author = relationship('Authors', back_populates='Books')
 
 class Authors(Base):
@@ -16,8 +16,8 @@ class Authors(Base):
 
     author_name= Column(String, primary_key=True, index=True)
     biography = Column(String, index=True)
-    genre = Column(Integer, primary_key=True, index=True)
-    description = Column(Integer, primary_key=True, index=True)
+    genre = Column(Integer, index=True)
+    description = Column(Text,  index=True)
     books = relationship('Books', back_populates='Authors')
 
 class Users(Base):
@@ -26,7 +26,7 @@ class Users(Base):
     username= Column(String, primary_key=True, index=True)
     password = Column(String, index=True)
     role = Column(String, index=True)
-    description = Column(Integer, primary_key=True, index=True)
+    description = Column(Text,  index=True)
     preferences = relationship('UserPreferences', back_populates='Users')
 
 
